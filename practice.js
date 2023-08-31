3cb21f37bd802130e1baf9584767814a9b74746f
ONUR <ok.onurkocak@gmail.com>
2023-08-31 14:37:45 +0300

    day5sonu

---
+++ practice.js
@@ -0,0 +1,27 @@
+let numOne = 3
+let numTwo = 3
+console.log(numOne == numTwo)
+
+let js = 'javascript'
+let py = 'python'
+console.log(js == py)
+
+let nums = [1, 2, 3, 4, 5]
+nums[0] = 10
+console.log(nums)
+
+let nums1 = [1, 2, 3]
+let nums2 = [1, 2, 3]
+
+console.log (nums1 == nums2)
+
+let user1 = {
+    name: 'onur',
+    surname: 'kocak',
+}
+
+let user2 = {
+    name: 'onur',
+    surname: 'kocak',
+}
+console.log(user1 == user2)
