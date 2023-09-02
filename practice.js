5d4f37645a9091ea6ddd3c18b5d14853f547e0f9
ONUR <ok.onurkocak@gmail.com>
2023-09-02 13:41:36 +0300

    day 7 sonu

--- practice.js
+++ practice.js
@@ -106,8 +106,84 @@ const secondList = [4, 5, 6, 10]
 let result = firstList.concat(secondList)
 console.log(result)
 
-firstList.push(11)
+firstList.push(11) //diziye yenisini eklemek
 console.log(firstList)
 firstList.pop()
 console.log(firstList)
 
+for (let i = 0; i <=100; i++){
+    console.log(`${i} * ${i} = ${i * i}`)
+}
+
+const names = ['onur', 'furkan', 'berat', 'berke', 'alperen','emre']
+for (let i = 0; i <= 5; i++){
+    console.log(names[i])
+}
+
+let countries = ['turkey', 'finland', 'spain', 'portugal', 'cyprus']
+let newCountries = []
+for (let i = 0; i < countries.length; i++){
+    newCountries.push(countries[i].toUpperCase())// touppercase (büyük harfle yazdırma) / (tolowercase küçük yazdırma)
+}
+console.log(newCountries)
+
+function sayHello(){
+    let msg = 'hello world!'
+    return ('hello!')
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+let total = multiply(5, 2)
+console.log('Toplam', total)
+
+
+/*let sayı = 10,
+    sayı2 = 4
+    let toplam = multiply(sayı, sayı2)
+*/
+    function sumArrayValues(array){
+        let esit = 0
+       for (let i = 0; i < array.length; i++){
+        esit += array[i]
+       }
+       return esit
+    }
+    let dizi = [2, 4, 6, 8, 10, 100]
+    console.log('Toplam', sumArrayValues(dizi))
+
+    function topla(){
+        let total = 0
+        for (let i =0; i < arguments.length; i++){
+            total += arguments[i]
+        }
+        return total
+
+    }
+   console.log( topla(2, 5, 7, 9, 10))
+
+   const sayyHello = function(name){
+    return `Hello ${name}`
+   }
+   console.log(sayyHello('onur'));
+
+   (function(){
+    console.log('hey !')
+   })()
+
+   let sayHello2 = (function(name){
+    console.log(`hello ${name}`)
+   })('onur')
+   
+   // normal fonksiyon
+   function hello(name){
+    return `hello ${name}`
+
+   }
+
+   //ok fonksiyonu
+   const hello2 = name => `hello ${name}`
+   console.log(hello2('kamil'))
+
+   
